b4ab5b69ce93d1ef396250f684a058719552ea84
GTPSHAX <rafiehasannudin@gmail.com>
2026-04-18 21:32:25 +0700

    fix: standardize formatting and improve consistency in docx-cover-page.js

--- src/scripts/docx-cover-page.js
+++ src/scripts/docx-cover-page.js
@@ -1,13 +1,17 @@
-import { TableWrapper } from './docx-api.js'
+import { ImageRun, AlignmentType } from 'docx'
+import { createParagraph, TableWrapper } from './docx-api.js'
+import { properties } from './docx-config.js'
+import fs from 'fs'
+import path from 'path'
 
 const coverPage = {
   properties,
   children: [
     // Blank space
-    createParagraph("", {
+    createParagraph('', {
       before: 3000,
       line: 240,
-      lineRule: "AUTO",
+      lineRule: 'AUTO'
     }),
     // Image tut wuri handayani
     createParagraph(
@@ -15,28 +19,32 @@ const coverPage = {
         children: [
           new ImageRun({
             data: fs.readFileSync(
-              path.join(__dirname, "../../assets/tut-wuri-handayani.png"),
+              path.join(__dirname, '../../assets/tut-wuri-handayani.png')
             ),
             transformation: {
               width: 200,
-              height: 200,
-            },
-          }),
+              height: 200
+            }
+          })
         ],
-        alignment: AlignmentType.CENTER,
+        alignment: AlignmentType.CENTER
       },
       {
         after: 400,
-        lineRule: "AUTO",
-      },
+        lineRule: 'AUTO'
+      }
     ),
     new TableWrapper()
-      .addTitleRow("MODUL AJAR\nKURIKULUM MERDEKA (Deep Learning)")
-      .addFormFieldRow("Nama Sekolah")
-      .addFormFieldRow("Nama Penyusun")
-      .addFormFieldRow("NIP")
-      .addFormFieldRow("Tema / Subtema")
-      .addFormFieldRow("Fase / Kelas / Semester")
-      .build(),
-  ],
-};
+      .addTitleRow('MODUL AJAR\nKURIKULUM MERDEKA (Deep Learning)')
+      .addFormFieldRow('Nama Sekolah')
+      .addFormFieldRow('Nama Penyusun')
+      .addFormFieldRow('NIP')
+      .addFormFieldRow('Tema / Subtema')
+      .addFormFieldRow('Fase / Kelas / Semester')
+      .build()
+  ]
+}
+
+export {
+  coverPage
+}
