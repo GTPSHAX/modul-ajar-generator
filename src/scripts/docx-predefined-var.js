4401c442b62fc941efc25d5ed2b30007f0b9e09c
GTPSHAX <rafiehasannudin@gmail.com>
2026-04-18 21:31:33 +0700

    feat: define predefined variables for document generation in docx-predefined-var.js

--- src/scripts/docx-predefined-var.js
+++ src/scripts/docx-predefined-var.js
@@ -1,7 +1,25 @@
-/* eslint-disable no-unused-vars */
-
-const temaSubtema = 'Identitas / Diriku'
+const namaPenulis = 'Nikola Tesla S.Pd.'
+const asalSekolah = 'TK Terpadu Harapan Bangsa'
+const fase = 'Fondasi'
+const jenjangKelas = 'Kelompok A (2-3 tahun)'
+const modelPembelajaran = 'Kolaboratif, Eksperimental'
+const topikSubtopik = 'Identitas / Diriku (Aku Istimewa: Ayo Kita Berkenalan)'
 const semester = 1
 const mingguKe = 1
+const alokasiWaktu = '5 x 3 JP'
+const jumlahAnak = 10
+const bulan = 'Januari 2026'
 
-/* eslint-enable no-unused-vars */
+export {
+  namaPenulis,
+  asalSekolah,
+  fase,
+  jenjangKelas,
+  modelPembelajaran,
+  topikSubtopik,
+  semester,
+  mingguKe,
+  alokasiWaktu,
+  jumlahAnak,
+  bulan
+}
