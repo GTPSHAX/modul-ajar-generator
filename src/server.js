fbf236c31775168b667cc3d8d34e45e65a2ac467
GTPSHAX <rafiehasannudin@gmail.com>
2026-04-22 13:09:30 +0700

    fix: enhance server logging with consola for better output

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import app from './index.js'
+import consola from 'consola'
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on http://localhost:3000')
+const server = app.listen(3000, '0.0.0.0', () => {
+  const addressInfo = server.address()
+  consola.log(`Bound to: ${addressInfo.address} on port ${addressInfo.port}`)
+  consola.success('Server is running on http://localhost:3000')
 })
