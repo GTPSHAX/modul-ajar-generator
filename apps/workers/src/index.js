dce22bd501de038bf558d1a25dad347b2fc231de
GTPSHAX <rafiehasannudin@gmail.com>
2026-05-06 10:45:22 +0700

    feat: implement POST request handling for autofill and document generation

--- apps/workers/src/index.js
+++ apps/workers/src/index.js
@@ -1,11 +1,11 @@
+import handleAutoFillAI from '@repo/handlers/handle-autofill-ai.js'
+import handleGenerateDocx from '@repo/handlers/handle-generate-docx.js'
+
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * @typedef {Object} ResultOfHandler
+ * @property {number} status - The HTTP status code to return in the response
+ * @property {string | undefined} [data] - The data to include in the response body
+ * @property {string | undefined} [message] - A message to include in the response body
  */
 
 export default {
@@ -16,13 +16,32 @@ export default {
    */
   async fetch (request, env, ctx) {
     const url = new URL(request.url)
-    switch (url.pathname) {
-      case '/message':
-        return new Response('Hello, World!')
-      case '/random':
-        return new Response(crypto.randomUUID())
-      default:
-        return new Response('Not Found', { status: 404 })
+
+    if (request.method === 'POST') {
+      try {
+        const body = await request.json()
+        /** @type {ResultOfHandler} */
+        let result = {
+          status: 500,
+          data: undefined,
+          message: 'Something went wrong. Please try again later.'
+        }
+
+        console.debug('Received request with body:', body)
+
+        if (url.pathname === '/api/autofill-ai') {
+          result = await handleAutoFillAI(body)
+        } else if (url.pathname === '/api/generate-docx') {
+          result = await handleGenerateDocx(body)
+        } else {
+          return new Response('Not Found', { status: 404 })
+        }
+
+        return new Response(result.data ?? result.message, { status: result.status })
+      } catch (/** @type {Error | unknown} */ error) {
+        console.error('Error occurred while processing request:', error)
+        return new Response('Internal Server Error', { status: 500 })
+      }
     }
   }
 }
