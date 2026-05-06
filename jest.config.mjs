66b75b7510eb678b35fe9324e24b1d2dc5317a4e
GTPSHAX <rafiehasannudin@gmail.com>
2026-05-06 10:08:58 +0700

    fix: correct coverageProvider syntax and update testPathIgnorePatterns

--- jest.config.mjs
+++ jest.config.mjs
@@ -32,7 +32,7 @@ const config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -162,9 +162,10 @@ const config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/apps/workers/test/'
+  ]
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
